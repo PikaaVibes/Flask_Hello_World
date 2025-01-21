0fe5c4b861bc4ee0a24571c0727183585c9126b1
PikaaVibes <schiesserloic123@gmail.com>
2025-01-21 14:44:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,11 @@ from flask import json
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+@app.route('/')
+def accueil():
+    return render_template('hello.html')
+
 @app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
